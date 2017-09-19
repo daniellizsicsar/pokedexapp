4ab6eeeaf778233f2e6ea0d266f17f5c2095f99e
Daniel Lizsicsar <lizsismac@Daniels-MBP.lan>
2017-09-19 19:40:48 +0200

    error with unwrapping

---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Daniel Lizsicsar on 16/09/2017.
+//  Copyright © 2017 Daniel Lizsicsar. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
