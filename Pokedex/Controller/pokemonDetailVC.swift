93999e66289e0e25b99d984dd222b242062b2527
Daniel Lizsicsar <lizsismac@Daniels-MBP.lan>
2017-09-21 21:18:49 +0200

    back button working

--- Pokedex/Controller/pokemonDetailVC.swift
+++ Pokedex/Controller/pokemonDetailVC.swift
@@ -13,6 +13,20 @@ class pokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLBl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
     
 
     override func viewDidLoad() {
@@ -22,6 +36,9 @@ class pokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
 
 }
